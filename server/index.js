f3daf09586aeb4480541a8380d91a99651a23706
chienchih <chien@xiaoxianroude-MacBook-Pro.local>
2019-10-12 17:25:40 -0700

    fix the server port

--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const path = require('path');
 const db = require('../database/insertDatabase');
 const app = express();
-const PORT = 8080;
+const PORT = 5000;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/imagetest', {useNewUrlParser: true});
+const distPath = path.resolve(__dirname, '..', 'dist');
 
 // req.url and the req.method
 app.use((req, res, next) => {
   console.log(`${req.method} request for ${req.url}`);
   next();
 });
-app.use(express.static('public'));
+app.use(express.static(distPath));
 app.use(express.json());
 
-app.get( '/server', (req, res)=> {
+app.get( '/images', (req, res)=> {
   res.send('got the request');
+  // req.params = (/imagesID)
+  // req.body = ();
+  // after images = '';
+});
+
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is listening on port: ${PORT}`);
 });
